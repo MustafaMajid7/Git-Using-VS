71d6aeb16d03f95fd92529d0b6a4d9b7d98f3a93
mostafa maged <eng.mustafamaged98@gmail.com>
2023-11-27 02:02:26 +0200

    adding welcome message

--- VSGIT/Program.cs
+++ VSGIT/Program.cs
@@ -5,6 +5,8 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello from GIT World!");
-		}
+			string name = "mostafa";
+            Console.WriteLine(name + " ,and my age is 25 years old");
+        }
 	}
 }
