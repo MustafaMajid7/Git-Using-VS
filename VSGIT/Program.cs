f65b4ae08a23d4054a522d55c4f09fc2d380e10e
mostafa maged <eng.mustafamaged98@gmail.com>
2023-11-27 02:19:40 +0200

    adding date time of now

--- VSGIT/Program.cs
+++ VSGIT/Program.cs
@@ -5,8 +5,11 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello from GIT World!");
-			string name = "mostafa";
+			string name = "master";
             Console.WriteLine(name + " ,and my age is 25 years old");
+
+			var date = DateTime.Now;
+            Console.WriteLine($"we are in the year of {date.Year} and month is {date.Month}");
         }
 	}
 }
