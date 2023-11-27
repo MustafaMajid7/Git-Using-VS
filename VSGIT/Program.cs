d423d7d6ced642a5eb16dea4faa589f3e905d0d2
mostafa maged <eng.mustafamaged98@gmail.com>
2023-11-27 03:19:27 +0200

    adding enum of seasons

--- VSGIT/Program.cs
+++ VSGIT/Program.cs
@@ -5,15 +5,15 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello from GIT World!");
-			string name = "mahmoud";
-            Console.WriteLine(name + " ,and my age is 28 years old");
 			string name = "master";
+            Console.WriteLine(name + " ,and my age is 28 years old");
             Console.WriteLine(name + " ,and my age is 25 years old");
 
 			var date = DateTime.Now;
             Console.WriteLine($"we are in the year of {date.Year} and month is {date.Month}");
 
             Console.WriteLine(capitaliseWord("mostafa"));
+            Console.WriteLine(Seasons.Summer);
         }
 		static string capitaliseWord( string word )
 		{
---
+++ VSGIT/Seasons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSGIT
+{
+	public enum Seasons
+	{
+		Summer=1,
+		Winter=2,
+		Automn=3,
+		Spring=4
+	}
+}
